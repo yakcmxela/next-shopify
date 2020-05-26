d6f4f7db798f7aaf340b5db990596c6be01ad6d3
Alex McKay <alexmckay@MacBook-Pro-3.attlocal.net>
2020-05-26 18:50:40 -0400

    This is probably pointless

--- liquify.js
+++ liquify.js
@@ -6,40 +6,44 @@ const dir = `${__dirname}/out`
 const outs = ["", "layout", "sections", "templates"]
 const renames = []
 
-const read = async (subdir) => {
-  const fullPath = `${dir}/${subdir}`
-  try {
-    const files = await fs.readdirSync(fullPath)
-    files.forEach((file) => {
-      const split = file.split(".")
-      if (split[split.length - 1] === "html") {
-        split[split.length - 1] = "liquid"
-        renames.push({
-          current: `${fullPath}/${file}`,
-          expected: `${fullPath}/${split.join(".")}`,
-        })
+const init = async () => {
+  Promise.map(outs, async (out) => {
+    try {
+      await read(out)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+    .catch((error) => {
+      console.log(error)
+    })
+    .then(async () => {
+      try {
+        await liquify()
+        // await modules()
+      } catch (error) {
+        console.log(error)
       }
     })
-  } catch (error) {
-    console.log(error)
-  }
 }
 
 const liquify = () => {
-  Promise.map(renames, async (target) => {
+  Promise.each(renames, async (target) => {
     try {
       const file = await fs.readFileSync(target.current, { encoding: `UTF-8` })
-      const srcs = file.match(/src="\/\_next(.*?)"/)
-      const hrefs = file.match(/href="\/\_next(.*?)"/)
-      const srcsSplit = srcs[1].split("/")
-      const hrefsSplit = hrefs[1].split("/")
-      const lastSrc = srcsSplit[srcsSplit.length - 1]
-      const lastHref = hrefsSplit[hrefsSplit.length - 1]
-      await file.replace(new RegExp(srcs[1]), lastSrc)
-      await file.replace(new RegExp(hrefs[1]), lastHref)
-      console.log(srcs[1])
-      console.log(file)
+      const hrefs = file.match(new RegExp(/href="\/\_next(.*?)"/, "g"))
+      const srcs = file.match(new RegExp(/src="\/\_next(.*?)"/, "g"))
+      await extra.ensureDirSync(`${dir}/assets`)
+      let newFile = file
+      newFile = await moveAndRename(dir, hrefs, newFile, "href")
+      newFile = await moveAndRename(dir, srcs, newFile, "src")
+      const schema = file.match(new RegExp(/{% schema %}(.*?){% endschema %}/))
+      if (schema) {
+        newFile.replace(new RegExp(schema[1], ""))
+      }
+      await fs.writeFileSync(target.current, newFile)
       await fs.renameSync(target.current, target.expected)
+      // await extra.removeSync(`${dir}/_next`)
     } catch (error) {
       console.log(error)
     }
@@ -57,25 +61,43 @@ const liquify = () => {
     })
 }
 
-const init = async () => {
-  Promise.map(outs, async (out) => {
-    try {
-      await read(out)
-    } catch (error) {
-      console.log(error)
+const moveAndRename = async (dir, arr, file, type) => {
+  let newFile = file
+  for (const i in arr) {
+    const filePath = arr[i].match(/"((?:\\.|[^"\\])*)"/)[1]
+    const split = filePath.split("/")
+    newFile = newFile.replace(
+      new RegExp(arr[i], "g"),
+      `${type}="{{ '${split[split.length - 1]}' | asset_url }}"`
+    )
+    const pathExists = await extra.pathExistsSync(`${dir}${filePath}`)
+    if (pathExists) {
+      await extra.moveSync(
+        `${dir}${filePath}`,
+        `${dir}/assets/${split[split.length - 1]}`
+      )
     }
-  })
-    .catch((error) => {
-      console.log(error)
-    })
-    .then(async () => {
-      try {
-        await liquify()
-        await modules()
-      } catch (error) {
-        console.log(error)
+  }
+  return newFile
+}
+
+const read = async (subdir) => {
+  const fullPath = `${dir}/${subdir}`
+  try {
+    const files = await fs.readdirSync(fullPath)
+    files.forEach((file) => {
+      const split = file.split(".")
+      if (split[split.length - 1] === "html") {
+        split[split.length - 1] = "liquid"
+        renames.push({
+          current: `${fullPath}/${file}`,
+          expected: `${fullPath}/${split.join(".")}`,
+        })
       }
     })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 init()
